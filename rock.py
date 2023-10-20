60584ae0fa9f284adb76468f85aabf27944c7f8a
Javier Mercado-Peralta <JJMercadoPeralta@student.fullsail.edu>
2023-10-20 15:46:13 -0400

    fixed errors

--- rock.py
+++ rock.py
@@ -10,43 +10,44 @@ while True:
     cpu_options = ["rock", "paper", "scissors"]
     player_options = ["rock", "paper", "scissors"]
 
-    player_input = input("Choose your weapon: 'rock', 'paper', or 'scissors' ")
+    player_input = input("Choose your weapon: 'rock', 'paper', or 'scissors' or 'quit'")
     if player_input.strip().lower() == 'rock': #if player typed in rock
         pOption = 'rock'
 
 
-    def check_winner(option):
-       if option.lower() == "rock" and random.choice(cpu_options) == "paper":
-        print(f"You lose,cpu picked {random.choice(cpu_options)}" )
-       elif option.lower() == "rock" and random.choice(cpu_options) == "rock":
-        print("Tie")
-       elif option.lower() == "rock" and random.choice(cpu_options) == "scissors":
-        print("You win! FINALLY!")
+        def check_winner(option):
+            if option.lower() == "rock" and random.choice(cpu_options) ==   "paper":
+                print(f"You lose, CPU picked {random.choice(cpu_options)}" )
+            elif option.lower() == "rock" and random.choice(cpu_options) == "rock":
+                print("Tie")
+            elif option.lower() == "rock" and random.choice(cpu_options) == "scissors":
+                print("You win! FINALLY!")
         
-       elif option.lower() == "paper" and random.choice(cpu_options) == "paper":
-        print("Tie")
-       elif option.lower() == "paper" and random.choice(cpu_options) == "rock":
-        print("You win! FINALLY!")
-       elif option.lower() == "paper" and random.choice(cpu_options) == "scissors":
-        print(f"You lose,cpu picked {random.choice(cpu_options)}")
+            elif option.lower() == "paper" and random.choice(cpu_options) == "paper":
+                print("Tie")
+            elif option.lower() == "paper" and random.choice(cpu_options) == "rock":
+                print("You win! FINALLY!")
+            elif option.lower() == "paper" and random.choice(cpu_options) == "scissors":
+                print(f"You lose, CPU picked {random.choice(cpu_options)}")
+            elif option.lower() == "scissors" and random.choice(cpu_options) == "paper":
+                print("You win! FINALLY!")
+            elif option.lower() == "scissors" and random.choice(cpu_options) == "rock":
+                print(f"You lose, CPU picked {random.choice(cpu_options)}")
+            elif option.lower() == "scissors" and random.choice(cpu_options) == "scissors":
+                print("Tie")
+            elif option.lower() == "quit":
+                break 
+            else:
+                print("Don't stir away from the game player, choose an option")
+        check_winner(player_input)
+    else:
+        print("Thank you for playing!!! ")
+        break
 
-       elif option.lower() == "scissors" and random.choice(cpu_options) == "paper":
-        print("You win! FINALLY!")
-       elif option.lower() == "scissors" and random.choice(cpu_options) == "rock":
-        print(f"You lose,cpu picked {random.choice(cpu_options)}")
-       elif option.lower() == "scissors" and random.choice(cpu_options) == "scissors":
-        print("Tie")
-
-    check_winner(player_input)
- 
-
-else:
-    print("Thank you for playing!!! ")
 
 
 
-
- # if pOption.lower() == "rock" and random.choice(cpu_options) == "paper":
+# if pOption.lower() == "rock" and random.choice(cpu_options) == "paper":
     #     print("You lose")
     # elif pOption.lower() == "rock" and random.choice(cpu_options) == "rock":
     #     print("Tie")
