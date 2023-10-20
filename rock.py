6e44828b03bf9dfe5e9ba41cec09bdbb0e04de95
Javier Mercado-Peralta <JJMercadoPeralta@student.fullsail.edu>
2023-10-20 13:05:45 -0400

    PLayer and CPU options done, and if check for player option 'rock' finished

--- rock.py
+++ rock.py
@@ -1,1 +1,24 @@
 # let's do it
+import random
+
+print("Welcome") 
+start = input("Type 'start' to start the game or type 'quit' to quit game ")
+if start.lower() == "start":
+    print("Game Started")
+else:
+    print("Quitted Game")
+
+cpu_options = ["rock", "paper", "scissors"]
+player_options = ["rock", "paper", "scissors"]
+
+player_input = input("Choose your weapon: 'rock', 'paper', or 'scissors' ")
+if player_input.strip().lower() == 'rock': #if player typed in rock
+        pOption = 'rock'
+
+if pOption.lower() == "rock" and random.choice(cpu_options) == "paper":
+    print("You lose")
+elif pOption.lower() == "rock" and random.choice(cpu_options) == "rock":
+    print("Tie")
+elif pOption.lower() == "rock" and random.choice(cpu_options) == "scissors":
+    print("You win! FINALLY!")
+
